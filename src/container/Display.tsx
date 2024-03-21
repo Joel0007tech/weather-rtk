0e702c746b166bec5f75c3bcd47cf1aa4fff0e4e
Joel <ajiolajoel@gmail.com>
2024-03-21 15:45:25 +0100

    update to weather

--- src/container/Display.tsx
+++ src/container/Display.tsx
@@ -4,11 +4,37 @@ import React from 'react'
   
 const Head = () => {
   return (
-    <div className='bg-gray-200 p-5'>
-      <div className='flex justify-center'>
-      <h1 className="text-blue-700 text-4xl">A weather <span className='text-yellow-300'>
+    <div className='bg-gray-200 p-5 flex justify-center'>
+      <header className='flex flex-col justify-center'>
+      <div className='flex flex-col'>
+      <h1 className="text-blue-700 text-4xl"> <span className='text-black'>A </span>
+      weather <span className='text-black'>
         A</span>pp</h1>
-      </div>
+        <p>Your friendly weather forecast finder</p>
+        </div>
+      </header>
+     
+        <section>
+          {/* <div className=' flex justify-center row gap-5'>
+          <div className='flex flex-col p-4 bg-blue-700'>
+           <h3>Hello</h3>
+           <p>Your text here</p>
+           <a href="http://">Link here</a>
+          </div>
+          <div className='flex flex-col p-4 bg-blue-700'>
+           <h3>Hello</h3>
+           <p>Your text here</p>
+           <a href="http://">Link here</a>
+          </div>
+          <div className='flex flex-col p-4 bg-blue-700'>
+           <h3>Hello</h3>
+           <p>Your text here</p>
+           <a href="http://">Link here</a>
+          </div>
+          </div> */}
+
+          <input type="text" name="" id="" />
+        </section>
     
     </div>
   )
