d862615c673a13a2d8fa59e7d09bec983d3a4a26
Joel <ajiolajoel@gmail.com>
2024-03-23 08:23:22 +0100

    update to weather

--- src/container/Display.tsx
+++ src/container/Display.tsx
@@ -7,20 +7,22 @@ const Head = () => {
   const myStyle = {
     backgroundImage:
         "url('https://images.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg')",
-    height: "50vh",
+    height: "109vh",
     marginTop: "-70px",
     backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    opacity:"0.7"
+    backgroundRepeat: "no-repeat"
 };
+const myFont ={
+  fontFamily:"cursive"
+}
   return (
-    <div className='p-32 bg-black' style={myStyle}>
+    <div className='p-32 pt-80' style={myStyle}>
       <header>
       <div>
-      <h1 className="text-blue-700 text-4xl text-center"> <span className='text-white'>A </span>
-      weather <span className='text-white'>
-        A</span>pp</h1>
-        <p className='text-center text-md pt-5 text-black'>Your friendly weather forecast finder</p>
+      <h1 className="text-black text-4xl text-center"> A
+      weather <span className='text-blue-700'>
+        App</span></h1>
+        <p className='text-center text-md pt-5 text-black' style={myFont}>Your friendly weather forecast finder</p>
         </div>
       </header>
      
@@ -43,9 +45,9 @@ const Head = () => {
           </div>
           </div> */}
 
-          <input type="text" name="" id="" className='bg-blue-600 w-4/12 h-14 p-3 border rounded-md'/>
+          <input type="text" name="" id="" className='bg-blue-600 w-4/12 h-14 p-3 border rounded-md' style={myFont}/>
           <CiSearch className=' relative right-11 top-4 size-6 text-white'/>
-          <input type="submit" value="Search" className=' cursor-pointer bg-blue-600 p-2 pr-10 pl-10 border rounded text-md text-white'/>
+          <input type="submit" value="Search" className=' cursor-pointer bg-blue-600 p-2 pr-10 pl-10 border rounded text-md text-white' style={myFont}/>
         </section>
     
     </div>
