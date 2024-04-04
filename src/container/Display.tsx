6a6489413610ed87557e33192cbdecbc212579c1
Joel <ajiolajoel@gmail.com>
2024-04-04 15:35:46 +0100

    update to weather

--- src/container/Display.tsx
+++ src/container/Display.tsx
@@ -24,7 +24,7 @@ const myFont ={
 const {data, error, isLoading } = useGetWeatherQuery('Miami')
   return (
     <div className='p-32 pt-80' style={myStyle}>
-      {/* <header>
+      {<header>
       <div>
       <h1 className="text-black text-4xl text-center font-bold"> A
       <span className='text-blue-700'> weather 🌐 </span>
@@ -32,46 +32,13 @@ const {data, error, isLoading } = useGetWeatherQuery('Miami')
         <p className='text-center text-md pt-5 tracking-widest font-semibold ' style={myFont}>Your friendly 
         <span className='text-black font-extrabold'> weather</span> forecast finder</p>
         </div>
-      </header> */}
-      <div className=' relative top-16'>
-      <Selector/>
-        </div>
+      </header>}
 
-        {/* <section className='flex justify-center pt-5 gap-3'> */}
-          {/* <div className=' flex justify-center row gap-5'>
-          <div className='flex flex-col p-4 bg-blue-700'>
-           <h3>Hello</h3>
-           <p>Your text here</p>
-           <a href="http://">Link here</a>
-          </div>
-          <div className='flex flex-col p-4 bg-blue-700'>
-           <h3>Hello</h3>
-           <p>Your text here</p>
-           <a href="http://">Link here</a>
-          </div>
-          <div className='flex flex-col p-4 bg-blue-700'>
-           <h3>Hello</h3>
-           <p>Your text here</p>
-           <a href="http://">Link here</a>
-          </div>
-          </div> */}
-{/* 
-          <select className='bg-blue-600 w-4/12 h-14 p-3 border rounded-md text-white' style={myFont}>
-          <option value=""></option>
-          </select>
-          <input type="submit" value=" Search " className=' cursor-pointer bg-blue-600 p-2 pr-8 
+      <section className='flex flex-row gap-4 justify-center relative top-3'>
+      <Selector/>
+      <input type="submit" value=" Search " className=' cursor-pointer bg-blue-600 pr-8 h-9 relative top-1
           pl-8 border rounded text-md text-white hover:bg-black text-white;' style={myFont}/>
-            <CiSearch className=' relative right-7 top-4 size-6 text-white'/>
         </section>
-    {error? (
-      <>oh, bla bla</>
-    ): isLoading ? (
-      <>....loading</>
-    ): data ?(
-      <>
-      <h3 className='text-white'>{data.Miami}</h3></>
-    ):null}
-    */}
     </div>
   )
 }
--- src/container/Selector.tsx
+++ src/container/Selector.tsx
@@ -29,7 +29,8 @@ export default function Selector() {
     <div>
       <Combobox value={selected} onChange={setSelected}>
         <div className="relative mt-1 w-72 m-auto">
-          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2
+           focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:to-blue-500 sm:text-sm">
             <Combobox.Input
               className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
               displayValue={(person) => person.name}
